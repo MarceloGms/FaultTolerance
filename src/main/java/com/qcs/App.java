f72f891d2997dbdf86c0817b955173a3a48d0fc2
Marcelo Gomes <mars.gomes2003@gmail.com>
2025-03-24 14:39:12 +0000

    classes

--- src/main/java/com/qcs/App.java
+++ src/main/java/com/qcs/App.java
@@ -1,9 +1,10 @@
 package com.qcs;
 
-/**
- * Hello world!
- *
- */
+/* problems:
+software bugs
+errors passing parameters calling our function like wrong order
+sensors can break and give us wrong data like 10000
+radiation from the sun can eat the cpu (soft errors)  */
 public class App 
 {
     public static void main( String[] args )
---
+++ src/main/java/com/qcs/CruiseControl.java
@@ -0,0 +1,6 @@
+package com.qcs;
+
+public class CruiseControl {
+   
+}
+
